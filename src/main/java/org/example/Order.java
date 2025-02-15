a8286a18630785e374737ee01e96ed748c1102ef
Дария <dariazhogibek@gmail.com>
2025-02-15 22:49:59 +0500

    new class Order

---
+++ src/main/java/org/example/Order.java
@@ -0,0 +1,34 @@
+package org.example;
+
+public class Order {
+    private final String orderId;
+    private final String customerName;
+    private String status;
+
+    public Order(String orderId, String customerName, String status) {
+        this.orderId = orderId;
+        this.customerName = customerName;
+        this.status = "Pending";
+    }
+
+    public String getOrderId() {
+        return orderId;
+    }
+    public String getCustomerName() {
+        return customerName;
+    }
+    public String getStatus() {
+        return status;
+    }
+    public void setStatus(String status) {
+        this.status = status;
+    }
+    @Override
+    public String toString() {
+        return "Order{" +
+                "orderId='" + orderId + '\'' +
+                ", customerName='" + customerName + '\'' +
+                ", status='" + status + '\'' +
+                '}';
+    }
+}
